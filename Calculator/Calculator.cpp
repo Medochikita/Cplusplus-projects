8399d3dd03f9328e567419327063d4329610979f
Medochikita <kutil.ondra@outlook.cz>
2022-04-07 19:52:37 +0200

    Simple change

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -1,26 +1,6 @@
 #include <iostream>;
 using namespace std;
-
-float add(float a, float b)
-{
-	return a + b;
-}
-
-float substrack(float a, float b) 
-{
-	return a - b;
-}
-
-float divide(float a, float b)
-{
-	return a / b;
-}
-
-float multiply(float a, float b)
-{
-	return a * b;
-}
-
+#include "Functions.h";
 
 int main()
 {
---
+++ Calculator/Functions.h
@@ -0,0 +1,21 @@
+#pragma once
+
+float add(float a, float b)
+{
+	return a + b;
+}
+
+float substrack(float a, float b)
+{
+	return a - b;
+}
+
+float divide(float a, float b)
+{
+	return a / b;
+}
+
+float multiply(float a, float b)
+{
+	return a * b;
+}

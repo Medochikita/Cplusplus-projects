8f0fbc9b938f48ff0adb4468d8a7f36dc2c4b01f
Medochikita <kutil.ondra@outlook.cz>
2022-04-08 19:46:25 +0200

    Create IntroToPointers.cpp

---
+++ Intro To Pointers/IntroToPointers.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+void print(void* pointer, char type) {
+	switch (type)
+	{
+	case 'i': 
+		cout << *((int*)pointer) << endl; break;
+	case 'c':
+		cout << *((char*)pointer) << endl; break;
+	}
+}
+
+int main()
+{
+    
+	int n = 5;
+	char letter = 'c';
+
+	print(&n, 'i');
+	print(&letter, 'c');
+
+    return 0;
+}
